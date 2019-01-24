63a720fb79250063135419b034334849a46e6fc2
celine kaslin <celine.kaslin@gmail.com>
2019-01-24 15:22:32 +0000

    test for throw error case

--- squareSpec.js
+++ squareSpec.js
@@ -6,7 +6,23 @@ describe('Square class', function(){
     });
     it('has an area', function(){
       s = new Square(10);
-      expectEquality(s.area(), 100);
+      expectEquality(s.area(), 1000);
+      // this test is deliberately built with a wrong expectation, for the user to see the test failing.
+    });
+  });
+
+  summary('It can throws errors', function(){
+    it('Only works for numbers being input', function(){
+      var s = function(){
+        new Square('Cheese')
+      }
+      expectError(s,'argument is not a number');
+    });
+    it('Only works with positive number', function(){
+      var s = function(){
+        new Square(-3)
+      }
+      expectError(s, 'size should be a positive number');
     });
   });
 });
